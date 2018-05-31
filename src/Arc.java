2cebffd6ed69f169ea56287872f95d6243393134
Luis Magahaes <iqop@outlook.pt>
2018-05-31 14:44:07 +0100

    Neural network structure finalized, the network as all of the arcs and connections needed for back-propagation

--- src/Arc.java
+++ src/Arc.java
@@ -2,11 +2,13 @@ import java.util.Random;
 
 public class Arc {
     Perceptron origin;
+    Perceptron destination;
     double value;
     double weight;
 
-    Arc(Perceptron origin,double value){
+    Arc(Perceptron origin,double value,Perceptron destination){
         this.origin = origin;
+        this.destination = destination;
         this.value = value;
         Random r = new Random();
         this.weight = -1 + 2*r.nextDouble();
--- src/NeuralNetwork.java
+++ src/NeuralNetwork.java
@@ -9,31 +9,41 @@ public class NeuralNetwork {
     public static void main(String[] args){
         Perceptron input[] = new Perceptron[NUMBER_OF_INPUT_NODES];
         Perceptron hidden_layer[] = new Perceptron[NUMBER_OF_HIDDEN_LAYER_NODES];
-        Perceptron output;
+        Perceptron output = new Perceptron(NeuralNetwork::input_function,NeuralNetwork::activation_function);
 
         //Conjunto de treino 1
         int conjuntosTreino[][] = {{1,1,0,0},{1,0,0,0},{0,0,0,0},{1,1,1,0}};
         int solucoes[] = {1,0,1,0};
 
+        //Constroi as conexoes
+        buildNeuralNetwork(input,hidden_layer,output,conjuntosTreino);
+        System.out.println();
+    }
+
+    static void buildNeuralNetwork(Perceptron input[],Perceptron hidden_layer[],Perceptron output,int conjuntosTreino[][]){
+
+
         for(int i=0;i<NUMBER_OF_INPUT_NODES;i++){
             input[i]= new Perceptron(NeuralNetwork::input_function,NeuralNetwork::activation_function);
-            input[i].add_input_arc(new Arc(null,conjuntosTreino[0][i]));
-            /*
-                NO IDEA WHAT TO DO NEXT
-             */
+            input[i].add_input_arc(new Arc(null,conjuntosTreino[0][i],input[i]));
+
         }
         for(int i=0;i<NUMBER_OF_HIDDEN_LAYER_NODES;i++){
             hidden_layer[i] = new Perceptron(NeuralNetwork::input_function,NeuralNetwork::activation_function);
+            //For each output_node from
             for(int j=0;j<NUMBER_OF_INPUT_NODES;j++){
-                for(Arc a : input[j].output_links){
-                    hidden_layer[i].add_input_arc(a);
-                }
+                Arc connection = new Arc(input[j],0,hidden_layer[i]);
+                input[j].add_output_arc(connection);
+                hidden_layer[i].add_input_arc(connection);
             }
-            /*
-                NO IDEA WHAT TO DO NEXT
-             */
+            Arc outputConnection = new Arc(hidden_layer[i],0,output);
+            hidden_layer[i].add_output_arc(outputConnection);
+            output.add_input_arc(outputConnection);
+
         }
     }
+
+
 
     public static double activation_function(double value){
         return (double) 1 / (1+Math.pow(Math.E,(-value)));
