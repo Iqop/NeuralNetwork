23f606a4958a2fd0aa92e42e9d86c9db5d0b268d
Luis Magahaes <iqop@outlook.pt>
2018-06-03 21:35:06 +0100

    Finishing touches

--- src/Arc.java
+++ src/Arc.java
@@ -1,23 +1,23 @@
 import java.util.Random;
 public class Arc {
-    Perceptron origin;
-    Perceptron destination;
-    double value;
-    double weight;
-
-    Arc(Perceptron origin,double value,Perceptron destination){
-        this.origin = origin;
-        this.destination = destination;
-        this.value = value;
-        Random r = new Random();
-        this.weight = -1 + 2*r.nextDouble();
-    }
-
-
-    Arc(double value,Perceptron destination){
-        this.origin = null;
-        this.destination = destination;
-        this.value = value;
-        this.weight = 1;
-    }
+  Perceptron origin;
+  Perceptron destination;
+  double value;
+  double weight;
+  
+  Arc(Perceptron origin,double value,Perceptron destination){
+    this.origin = origin;
+    this.destination = destination;
+    this.value = value;
+    Random r = new Random();
+    this.weight = -1 + 2*r.nextDouble();
+  }
+  
+  
+  Arc(double value,Perceptron destination){
+    this.origin = null;
+    this.destination = destination;
+    this.value = value;
+    this.weight = 1;
+  }
 }
--- src/Perceptron.java
+++ src/Perceptron.java
@@ -3,43 +3,43 @@ import java.util.List;
 import java.util.function.Function;
 
 public class Perceptron {
-    List<Arc> input_links;
-    List<Arc> output_links;
-    Arc bias;
+  List<Arc> input_links;
+  List<Arc> output_links;
+  Arc bias;
+  
+  Function<List<Double>,Double> input_function;
+  Function<Double,Double> activation_function;
+  
+  double input_value;
+  double output;
+  double sigma;
+  
+  Perceptron(Function<List<Double>,Double> input_function, Function<Double,Double> activation_function){
+    this.input_function = input_function;
+    this.activation_function = activation_function;
+    input_links = new LinkedList<>();
+    output_links = new LinkedList<>();
+  }
+  
+  void add_input_arc(Arc arc){
+    input_links.add(arc);
+  }
+  void add_output_arc(Arc arc){
+    output_links.add(arc);
+  }
+  
+  void calc_output(){
+    LinkedList<Double> list = new LinkedList<>();
+    for(Arc arc : input_links){
+      list.add(arc.value * arc.weight);
+    }
+    input_value = input_function.apply(list);
+    input_value+=bias.value*bias.weight;
+    output = activation_function.apply(input_value);
     
-    Function<List<Double>,Double> input_function;
-    Function<Double,Double> activation_function;
-
-    double input_value;
-    double output;
-    double sigma;
-
-    Perceptron(Function<List<Double>,Double> input_function, Function<Double,Double> activation_function){
-        this.input_function = input_function;
-        this.activation_function = activation_function;
-        input_links = new LinkedList<>();
-        output_links = new LinkedList<>();
+    for (Arc a : output_links){
+      a.value = output;
     }
-
-    void add_input_arc(Arc arc){
-        input_links.add(arc);
-    }
-    void add_output_arc(Arc arc){
-        output_links.add(arc);
-    }
-
-    void calc_output(){
-        LinkedList<Double> list = new LinkedList<>();
-        for(Arc arc : input_links){
-            list.add(arc.value * arc.weight);
-        }
-        input_value = input_function.apply(list);
-        input_value+=bias.value*bias.weight;
-        output = activation_function.apply(input_value);
-
-        for (Arc a : output_links){
-            a.value = output;
-        }
-    }
-
+  }
+  
 }
